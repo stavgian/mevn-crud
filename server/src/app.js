dcc8aefc0b801e13da599073d7f2c788a8add197
Yiannis Stavgianoudakis <stavgian@gmail.com>
2018-11-08 00:06:43 +0100

    Full Crud

--- server/src/app.js
+++ server/src/app.js
@@ -8,15 +8,86 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+var mongoose = require('mongoose');
+var DATABASE_URL = process.env.DATABASE_URL || 'http://localhost'
+mongoose.connect(`mongodb://${DATABASE_URL}/vue-crud-demo`);
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
+
+var Post = require("../models/post");
 
 app.get('/posts', (req, res) => {
-    res.send(
-      [{
-        title: "Hello World!!! (nodemon)",
-        description: "Hi there! How are you?"
-      }]
-    )
+    Post.find({}, 'title description', function (error, posts) {
+        if (error) { console.error(error); }
+        res.send({
+              posts: posts
+          })
+      }).sort({_id:-1})
   })
+
+  app.post('/posts', (req, res) => {
+	var db = req.db;
+	var title = req.body.title;
+	var description = req.body.description;
+	var new_post = new Post({
+		title: title,
+		description: description
+	})
+
+	new_post.save(function (error) {
+		if (error) {
+			console.log(error)
+		}
+		res.send({
+			success: true
+		})
+	})
+})
+
+app.put('/posts/:id', (req, res) => {
+	var db = req.db;
+	Post.findById(req.params.id, 'title description', function (error, post) {
+	  if (error) { console.error(error); }
+
+	  post.title = req.body.title
+	  post.description = req.body.description
+	  post.save(function (error) {
+			if (error) {
+				console.log(error)
+            }
+            
+            console.log(post)
+			res.send({
+				success: true
+			})
+		})
+	})
+})
+
+app.delete('/posts/:id', (req, res) => {
+	var db = req.db;
+	Post.remove({
+		_id: req.params.id
+	}, function(err, post){
+		if (err)
+			res.send(err)
+		res.send({
+			success: true
+		})
+	})
+})
+
+app.get('/post/:id', (req, res) => {
+	var db = req.db;
+	Post.findById(req.params.id, 'title description', function (error, post) {
+	  if (error) { console.error(error); }
+	  res.send(post)
+	})
+})
   
   app.listen(process.env.PORT || 8081)
   
